49531326abfa326dd81f72107b1f3f684d184f3e
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2018-08-04 15:16:58 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,38 @@
+let myObj = {};
+let myArr = [];
+
+document.getElementById('myForm').addEventListener('submit', processForm, false);
+
+function processForm(e){
+    e.preventDefault();
+
+    const formData = new FormData(document.getElementById('myForm'));
+
+const inputArr = [0, 1].map(ind => document.querySelectorAll("input")[ind].value);
+  if (inputArr[0] !== "" && inputArr[1] !== ""){
+    for(let key of formData.entries()){
+        myObj[key[0]] = key[1];
+    }
+    myArr.push(myObj);
+
+    myObj = {};
+
+    document.getElementById('myForm').reset();
+    document.getElementById('grid-first-name').focus();
+    getResult();
+  }
+  }
+
+function getResult(){
+  let result = JSON.stringify(myArr);
+ result = result.replace(/\,(?={)/g, ',\n');
+document.getElementById('result').innerHTML = result;
+}
+// First form submit: [{"user":"test1","age":"40"}]
+// Second form submit: [{"user":"test1","age":"40"},{"user":"test2","age":"32"}]
+
+//clears everything one has typed
+function clearArray(){
+  myArr = [];
+  document.getElementById('result').innerHTML = '';
+}
